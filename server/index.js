668a461cab60c447c0dd48df00a942d24defe9c5
Mateen <mateen.muhammad@lyftyfy.com>
2024-07-19 20:18:19 +0500

    fix cors url

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 4000;
-
 //apis
 app.post('/register', async(req,res)=>{
     console.log('Received a registration request');
+    console.log('Connecting to MongoDB...', process.env.MONGODB_URI);
     try {
         const { name, email, password } = req.body;
         let existingUser = await Auth.findOne({email});
@@ -43,7 +43,7 @@ app.post('/register', async(req,res)=>{
         res.status(500).json({message: err.message});
     }
 })
-console.log('Connecting to MongoDB...');
+
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log('Connected!')
